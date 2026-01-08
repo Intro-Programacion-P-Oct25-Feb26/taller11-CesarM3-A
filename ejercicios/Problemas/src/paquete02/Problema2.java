0fd91c958d2c99d65d6ff62d73a587689dcad6a4
CesarM3-A <cesarmateoab2007@gmail.com>
2026-01-08 11:32:02 -0500

    Problema-2

--- ejercicios/Problemas/src/paquete02/Problema2.java
+++ ejercicios/Problemas/src/paquete02/Problema2.java
@@ -5,6 +5,8 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,47 @@ public class Problema2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        viviendas();
+        consumoMensual();
     }
+
+    public static String []viviendas() {
+        Scanner entrada = new Scanner(System.in);
+        String[] viviendas = new String[10];
+        int contador = 1;
+        for (int f = 0; f < 11; f++) {
+            System.out.println("Ingrese el nombre del dueño de la vivienda"
+                    + contador + ":");
+            viviendas[f] = entrada.nextLine();
+            contador = contador + 1;
+        }
+        return viviendas;
+    }
+    
+    public static String [][]consumoMensual(){
+        Scanner entrada = new Scanner(System.in);
+        String [][] consumo = new String[10][12];
+        int contador = 1;
+        for(int f = 0; f<11; f++){
+            for (int c = 0; f<13; c++){
+                System.out.println("Ingrese el consumo del mes"+contador+":");
+                contador = contador + 1;
+            }
+        }
+        return consumo;
+    }
+    
+    public static String []consumoTotal(double m){
+        String [] consumoT = new String[10];
+        double suma = 0; 
+        for(int f = 0; f<11; f++){
+            suma = 0;
+            for (int c = 0; f<13; c++){
+                suma = suma + m[f][c];
+            }
+           consumoT = suma;
+        }
+    }
+    
     
 }
